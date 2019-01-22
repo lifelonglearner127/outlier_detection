26217513b9e2e8f54c87cd1d2d5978c9f37c3707
lifelonglearner127 <life.long.learner127@outlook.com>
2019-01-22 19:14:50 +0400

    update verbose message

--- analyze_data.py
+++ analyze_data.py
@@ -24,47 +24,54 @@ def analyze_data(file_name):
     df['sub_system']=df['meta_name'].str.split('-').str[0]
     df['sensor']=df['meta_name'].str.split('-').str[1]
 
-    # Export sub systems
+    # Export all sub systems
     sub_systems = df['sub_system'].drop_duplicates()
     sub_systems = sub_systems.sort_values()
-    sub_systems.to_csv('sub_systems.csv', index=False)
+    sub_systems.to_csv('data/sub_systems.csv', index=False)
+    print('Finished exporting all sub-systems to data/sub_systems.csv')
 
-    # Export sensors
+    # Export all sensors
     sensors = df['sensor'].drop_duplicates()
     sensors = sensors.sort_values()
-    sensors.to_csv('sensors.csv', index=False)
+    sensors.to_csv('data/sensors.csv', index=False)
+    print('Finished exporting all sensors to data/sensors.csv')
 
     # Export sensors by sub systems
     for sub_system in sub_system_names:
         sub_sensors = df[df.sub_system == sub_system]['sensor']
         sub_sensors = sub_sensors.drop_duplicates()
         sub_sensors = sub_sensors.sort_values()
-        sub_sensors.to_csv(sub_system + '.csv', index=False)
+        sub_sensors.to_csv('data/' + sub_system + '_sensors.csv', index=False)
+        message = 'Finished exporting sensors from {0} to {1}_sensors.csv'.format(
+            sub_system, sub_system
+        )
+        print(message)
 
     # Observe the dataset we really cares about
     df = df[df['sub_system'].isin(sub_system_names)]
     
     # Export Images
     # Export bars
+    print('Started to exporting bar plot')
     bar_images_path = 'images/bars'
     if not os.path.exists(bar_images_path):
         os.makedirs(bar_images_path)
     
     for sensor in sub_sensors:
-        print('Generationg ' + sensor + '.png file' )
         dataset_by_sensor = df[df.sensor == sensor]
         counts = dataset_by_sensor.groupby('sub_system').agg(len)['buildingId']
         counts.plot(kind='barh')
         savefig(os.path.join(bar_images_path, sensor + '.png'))
         plt.clf()
+        print('Generated ' + sensor + '.png file')
 
     # Export Series
+    print('Started to exporting series plot')
     series_images_path = 'images/series'
     if not os.path.exists(series_images_path):
         os.makedirs(series_images_path)
 
     for sensor in sub_sensors:
-        print('Generationg ' + sensor + '.png file' )
         dataset_by_sensor = df[df.sensor == sensor]
         sns.lineplot(
             x="timestamp", y="value", hue="sub_system",
@@ -72,6 +79,7 @@ def analyze_data(file_name):
         )
         savefig(os.path.join(series_images_path, sensor + '.png'))
         plt.clf()
+        print('Generated ' + sensor + '.png file')
 
     return df
 
