7da6a37165f9cecbe3d950dc2c074329f1d95707
lifelonglearner127 <life.long.learner127@outlook.com>
2019-01-25 22:32:57 +0400

    update utility

--- extract_dataset.py
+++ extract_dataset.py
@@ -1,28 +1,28 @@
 import os
 import argparse
 import pandas as pd
-from pandas.io.json import json_normalize
 
 
-def extract_dataset(data_file, sub_system, sensor):
+sub_systems = [
+    '227001'
+]
+
+sensors = [
+    'Fluid temperature', 'Heat power', 'Flow'
+]
+
+def extract_dataset(data_file):
     df = pd.read_csv(data_file)
-    df.timestamp = pd.to_datetime(df.timestamp, format='%Y-%m-%d %H:%M:%S.%f')
     df['sub_system']=df['meta_name'].str.split('-').str[0]
     df['sensor']=df['meta_name'].str.split('-').str[1]
-    file_name = 'data/all.csv'
 
-    if sub_system == 'all' and sensor != 'all':
-        df = df[df.sensor == sensor]
-        file_name = 'data/{0}.csv'.format(sensor)
-    elif sensor == 'all' and sub_system != 'all':
-        df = df[df.sub_system == sub_system]
-        file_name = 'data/{0}.csv'.format(sub_system)
-    elif sensor != 'all' and sub_system != 'all':
-        df = df[(df.sub_system == sub_system) & (df.sensor == sensor)]
-        file_name = 'data/{0}_{1}.csv'.format(sub_system, sensor)
+    list_ = []
+    for sub_system in sub_systems:
+        for sensor in sensors:
+            list_.append(df[(df.sub_system == sub_system) & (df.sensor == sensor)])
 
-    df.to_csv(file_name, index=False)
-
+    df = pd.concat(list_, axis = 0, ignore_index = True)
+    df.to_csv('data/sub_data.csv', index=False)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -32,17 +32,6 @@ if __name__ == '__main__':
         default='data.csv',
         help='Specify the original data set'
     )
-    parser.add_argument(
-        '--subsystem',
-        type=str,
-        default='all',
-        help='Specify the sub-system name'
-    )
-    parser.add_argument(
-        '--sensor',
-        type=str,
-        default='all',
-        help='Specify the sensor'
-    )
+
     args = parser.parse_args()
-    extract_dataset(args.file, args.subsystem, args.sensor)
+    extract_dataset(args.file)
